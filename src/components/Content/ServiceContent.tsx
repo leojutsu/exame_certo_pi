0cf19974f356f86458aa54d70a8a58f9670ce37f
Leonardo <leojutsu@gmail.com>
2024-11-15 20:33:04 -0300

    Atualização de tags seviços disponíveis

--- src/components/Content/ServiceContent.tsx
+++ src/components/Content/ServiceContent.tsx
@@ -7,9 +7,9 @@ export default function ServiceContent() {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-screen-xl">
                 <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Service 1</h3>
+                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Agendamentos Online</h3>
                     <p className="text-gray-600 mb-4">
-                        A brief description of the service. Explain the benefits and features that make this service stand out.
+                        O paciente pode escolher a data e o horário mais convenientes para ele, evitando esperas prolongadas em clínicas ou hospitais.
                     </p>
                     <a
                         href="#"
@@ -20,9 +20,9 @@ export default function ServiceContent() {
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Service 2</h3>
+                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Notificações Automáticas</h3>
                     <p className="text-gray-600 mb-4">
-                        A brief description of the service. Highlight what makes this option a great choice for users.
+                        O sistema envia notificações (por e-mail, SMS ou aplicativo) para lembrar o paciente sobre consultas agendadas, coleta de exames, retorno de resultados e outros eventos importantes.
                     </p>
                     <a
                         href="#"
@@ -33,9 +33,9 @@ export default function ServiceContent() {
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
-                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Service 3</h3>
+                    <h3 className="text-2xl font-semibold text-gray-900 mb-4">Perfil de Paciente</h3>
                     <p className="text-gray-600 mb-4">
-                        A brief description of the service. Provide a clear explanation of the unique value proposition.
+                        O paciente pode acessar e visualizar seus exames a qualquer momento, através de um computador, tablet ou smartphone, de forma simples e intuitiva.
                     </p>
                     <a
                         href="#"
